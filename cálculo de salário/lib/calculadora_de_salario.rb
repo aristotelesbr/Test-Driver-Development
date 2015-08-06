defe71ba42e73b125343dc7bb618bd81c7ce9456
Aristóteles <aristotelesbr2014@gmail.com>
2015-08-06 12:03:56 -0300

    Completando o teste

--- cálculo de salário/lib/calculadora_de_salario.rb
+++ cálculo de salário/lib/calculadora_de_salario.rb
@@ -1,7 +1,29 @@
 class CalculadoraDeSalario
+
+#   Desenvolvedores possuem 20% de desconto caso seu salário seja maior
+# do que R$ 3000,0. Caso contrário, o desconto é de 10%.
+#   DBAs e testadores possuem desconto de 25% se seus salários forem
+# maiores do que R$ 2500,0. 15%, em caso contrário.
+
   def calcula_salario(funcionario)
-    return funcionario.salario * 0.85 if funcionario.salario < 1500
+    if funcionario.cargo == Cargo::DESENVOLVEDOR
+      return dez_ou_vinte_por_cento_de_desconto(funcionario)
+    elsif funcionario.cargo == Cargo::DBA ||
+          funcionario.cargo == Cargo::TESTADOR
+      return quinze_ou_vinte_e_cinco_por_cento_de_desconto(funcionario)
+    end
+    raise Exeption.new('Funcionario Inválido')
+  end
+
+  private
+
+  def dez_ou_vinte_por_cento_de_desconto(funcionario)
     return funcionario.salario * 0.8 if funcionario.salario > 3000
     return funcionario.salario * 0.9
+  end
+
+  def quinze_ou_vinte_e_cinco_por_cento_de_desconto(funcionario)
+    return funcionario.salario * 0.75 if funcionario.salario > 2500
+    return funcionario.salario * 0.85
   end
 end
--- cálculo de salário/lib/test.rb
+++ cálculo de salário/lib/test.rb
@@ -16,14 +16,26 @@ class CalculadoraDeSalarioTest < Test::Unit::TestCase
     calculadora = CalculadoraDeSalario.new
     desenvolvedor = Funcionario.new('Renato', 4000.0,
       Cargo::DESENVOLVEDOR)
+
     salario = calculadora.calcula_salario (desenvolvedor)
     assert_equal 4000 * 0.8, salario
   end
 
   def teste_deve_calcular_salario_para_DBAs_com_salario_abaixo_do_limite
     calculadora = CalculadoraDeSalario.new
-    desenvolvedor = Funcionario.new('Tio', 500.0, Cargo::DBA)
+    desenvolvedor = Funcionario.new('Ari', 1500.0,
+      Cargo::DBA)
+
     salario = calculadora.calcula_salario (desenvolvedor)
-    assert_equal 500 * 0.85, salario
+    assert_equal 1500 * 0.85, salario
+  end
+
+  def teste_deve_calcular_salario_para_DBAs_com_salario_acima_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Ari', 4000,
+      Cargo::DBA)
+
+    salario = calculadora.calcula_salario (desenvolvedor)
+    assert_equal 4000 * 0.75, salario
   end
 end
